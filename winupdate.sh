48c71b2792acc059502c7ca7bcb1ce3000730557
TAK-YON <48788608+TAK-YON@users.noreply.github.com>
2021-05-10 19:45:01 +1000

    😱 superior distro support

--- winupdate.sh
+++ winupdate.sh
@@ -12,6 +12,9 @@ fi
 if [ -e /usr/bin/dnf ]; then
     dnf -y upgrade
 fi
+if [-e /run/current-system/sw/bin/nixos-rebuild] then
+    nixos-rebuild boot --upgrade
+fi
 wall -n "Your PC will reboot in 30 seconds. No, you don't have a choice"
 sleep 30
 reboot
